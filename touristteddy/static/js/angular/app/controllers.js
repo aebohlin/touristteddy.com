bb6fe12f1e2c6c25520841c18b044676fccac99c
Anders Bohlin <aebohlin@gmail.com>
2013-05-10 13:25:31 +0200

    angular posts

--- touristteddy/static/js/angular/app/controllers.js
+++ touristteddy/static/js/angular/app/controllers.js
@@ -1,9 +1,20 @@
 'use strict';
 
-function PostCtrl($scope, $http) {
+//function PostCtrl($scope, $http) {
+//    $scope.init = function (url) {
+//        $http.get(url).success(function (data) {
+//            $scope.comments = data;
+//        });
+//    };
+//}
+
+function PostsCtrl($scope, $http) {
     $scope.init = function (url) {
         $http.get(url).success(function (data) {
-            $scope.comments = data;
+            $scope.posts = data;
         });
+    };
+    $scope.postComment = function (post) {
+        alert(post.title);
     };
 }
--- touristteddy/touristteddy/views.py
+++ touristteddy/touristteddy/views.py
@@ -6,23 +6,38 @@ from django.contrib.auth import authenticate, login
 from django.core import serializers
 from django.contrib.auth.models import User
 from touristteddy import utils
+import json
+
 
 def index(request):
-	teddys = Teddy.objects.all()
-	posts = Post.objects.all()
-	template = loader.get_template('index.html')
+    teddys = Teddy.objects.all()
+    posts = Post.objects.all()
+    template = loader.get_template('index.html')
 
-#c = RequestContext(request, {'message': 'I am view 1.'},
- #           processors=[custom_proc])
-	return HttpResponse(template.render(RequestContext(request, {
-		'teddys': teddys,
-		'posts': posts,
+    return HttpResponse(template.render(RequestContext(request, {
+        'teddys': teddys,
+        'posts': posts,
         'username': utils.get_username_or_fullname(request.user)
-		})))
-	#return HttpResponse(template.render(Context({
-	#	'teddys': teddys,
-	#	'posts': posts,
-	#	})))
+    })))
+
+
+def get_posts_as_json(request):
+    posts = Post.objects.all()
+    post_dictionary = [{
+                       'id': p.id,
+                       'title': p.title,
+                       'small_picture': p.small_picture.url,
+                       'picture': p.picture.url,
+                       'user_id': p.user.id,
+                       'user_name': p.user.username,
+                       'comments': [{
+                                    'comment': c.comment,
+                                    'comment_time': utils.get_friendly_time(c.comment_time),
+                                    'user_id': c.user.id,
+                                    'user_name': c.user.username, } for c in p.comment_set.all()], } for p in posts]
+    data = json.dumps(post_dictionary)
+    return HttpResponse(data, mimetype='application/json')
+
 
 def login_user(request):
     state = "Please log in below..."
@@ -31,20 +46,23 @@ def login_user(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         redirect_to = request.POST.get('next', '')
-		
+
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
                 login(request, user)
                 index(request)
                 state = "You're successfully logged in!"
-                return HttpResponseRedirect(redirect_to)  
+                return HttpResponseRedirect(redirect_to)
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Incorrent username and/or password."
 
-    return render_to_response('auth.html',{'state':state, 'username': username, 'next': request.REQUEST.get('next', '')}, context_instance=RequestContext(request))
+    return render_to_response('auth.html',
+                              {'state': state, 'username': username, 'next': request.REQUEST.get('next', '')},
+                              context_instance=RequestContext(request))
+
 
 def new_user(request):
     state = "Fill out the form below..."
@@ -57,7 +75,7 @@ def new_user(request):
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         redirect_to = request.POST.get('next', '')
-        
+
         user = User()
 
         if username == '':
@@ -68,7 +86,7 @@ def new_user(request):
             state = 'Passwords do not match'
         elif utils.validate_email_address(email) == False:
             state = 'Email is not valid'
-        else: 
+        else:
             user.username = username
             user.set_password(password)
             user.first_name = first_name
@@ -79,14 +97,14 @@ def new_user(request):
             login(request, user)
             index(request)
             state = "User created!"
-            return HttpResponseRedirect(redirect_to)  
+            return HttpResponseRedirect(redirect_to)
 
-    return render_to_response('newuser.html',{
-        'state':state, 
-        'username': username, 
-        'first_name': first_name, 
-        'last_name': last_name, 
-        'email': email, 
+    return render_to_response('newuser.html', {
+        'state': state,
+        'username': username,
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
         'next': request.REQUEST.get('next', '')
     }, context_instance=RequestContext(request))
 
@@ -94,4 +112,4 @@ def new_user(request):
 def get_user_as_json(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     data = serializers.serialize("json", user)
-    return HttpResponse({ "user": data } , mimetype='application/json')
+    return HttpResponse({"user": data}, mimetype='application/json')
